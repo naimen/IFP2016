15643d9ead81f393d51094fa01f27ec113d5dd34
Shuo Hua <naimen12@gmail.com>
2016-09-26 10:47:00 +0200

    test env

--- week-04/week-04_splitting-continuations.scm
+++ week-04/week-04_splitting-continuations.scm
@@ -4,6 +4,9 @@
 ;;; Version of 20 Sep 2016
 
 ;;;;;;;;;;
+(load "week-03_the-40-optimizing-compilers.scm")
+;;;;;;;;;;
+
 
 (define try-candidate
   (lambda (name candidate expected-output . input)
@@ -137,6 +140,19 @@
   (printf "fail: (test-well-balanced well-balanced?_v1)~n"))
 
 ;;;;;;;;;;
+(define does_interpret-arithmetic-expression_Magritte_surprising_make_the_diagram_commute?
+  (lambda (source-ae)
+    (let ([ae (parse-arithmetic-expression source-ae)])
+      (equal? (interpret-arithmetic-expression_Magritte_surprising_v2 ae)
+              (compile-and-run-arithmetic-expression_Magritte_surprising ae)))))
+
+(define test_does_interpret-arithmetic-expression_Magritte_surprising_make_the_diagram_commute?
+  (lambda ()
+    (andmap does_interpret-arithmetic-expression_Magritte_surprising_make_the_diagram_commute?
+            sample-of-arithmetic-expressions)))
+
+
+;;;;;;;;;;
 (define interpret-arithmetic-expression_Magritte_surprising_v0
   (lambda (e_init)
     (letrec ([visit (lambda (e)
@@ -145,25 +161,36 @@
                          (make-literal (literal-1 e))]
                         [(is-plus? e)
                          (let ([e1 (visit (plus-1 e))])
-                           (if (and (is-literal? e1) (= (literal-1 e1) 0))
+                           (if (and (is-literal? e1)
+                                    (= (literal-1 e1) 0))
                                (visit (plus-2 e))
+
                                (let ([e2 (visit (plus-2 e))])
-                                 (if (and (is-literal? e2) (= (literal-1 e2) 0))
+                                 (if (and (is-literal? e2)
+                                          (= (literal-1 e2) 0))
                                      e1
                                      (make-plus e1 e2)))))]
+
                         [(is-times? e)
                          (let ([e1 (visit (times-1 e))])
                            (cond
-                             [(and (is-literal? e1) (= (literal-1 e1) 0))
+                             [(and (is-literal? e1)
+                                   (= (literal-1 e1) 0))
                               (make-literal 0)]
-                             [(and (is-literal? e1) (= (literal-1 e1) 1))
+
+                             [(and (is-literal? e1)
+                                   (= (literal-1 e1) 1))
                               (visit (times-2 e))]
+
                              [else
                               (let ([e2 (visit (times-2 e))])
                                 (cond
-                                  [(and (is-literal? e2) (= (literal-1 e2) 0))
+                                  [(and (is-literal? e2)
+                                        (= (literal-1 e2) 0))
                                    (make-literal 0)]
-                                  [(and (is-literal? e2) (= (literal-1 e2) 1))
+
+                                  [(and (is-literal? e2)
+                                        (= (literal-1 e2) 1))
                                    e1]
                                   [else
                                    (make-times e1 e2)]))]))]))])
@@ -172,6 +199,7 @@
 (unless (test_does_interpret-arithmetic-expression_Magritte_surprising_make_the_diagram_commute?)
   (printf "fail: (test_does_interpret-arithmetic-expression_Magritte_surprising_make_the_diagram_commute?)~n"))
 
+;;;;;;;;
 (define interpret-arithmetic-expression_Magritte_surprising_v1
   (lambda (e_init)
     (letrec ([visit (lambda (e k)
@@ -181,11 +209,13 @@
                         [(is-plus? e)
                          (visit (plus-1 e)
                                 (lambda (e1)
-                                  (if (and (is-literal? e1) (= (literal-1 e1) 0))
+                                  (if (and (is-literal? e1)
+                                           (= (literal-1 e1) 0))
                                       (visit (plus-2 e) k)
                                       (visit (plus-2 e)
                                              (lambda (e2)
-                                               (if (and (is-literal? e2) (= (literal-1 e2) 0))
+                                               (if (and (is-literal? e2)
+                                                        (= (literal-1 e2) 0))
                                                    (k e1)
                                                    (k (make-plus e1 e2))))))))]
                         [(is-times? e)
@@ -210,38 +240,7 @@
 
 (unless (test_does_interpret-arithmetic-expression_Magritte_surprising_make_the_diagram_commute?)
   (printf "fail: (test_does_interpret-arithmetic-expression_Magritte_surprising_make_the_diagram_commute?)~n"))
-
-(define interpret-arithmetic-expression_Magritte_surprising_v2
-  (lambda (e_init)
-    (letrec ([visit (lambda (e k0 k1 kn)
-                      (cond
-                        [(is-literal? e)
-                         ...]
-                        [(is-plus? e)
-                         (visit (plus-1 e)
-                                (lambda ()
-                                  ...)
-                                (lambda ()
-                                  ...)
-                                (lambda (e1)
-                                  ...))]
-                        [(is-times? e)
-                         (visit (times-1 e)
-                                (lambda ()
-                                  ...)
-                                (lambda ()
-                                  ...)
-                                (lambda (e1)
-                                  ...))]))])
-      (visit e_init
-             (lambda ()
-               (make-literal 0))
-             (lambda ()
-               (make-literal 1))
-             (lambda (e)
-               e)))))
-
-
+;;;;;;;;;;;;
 
 ;;; end of week-04_splitting-continuations.scm
 
