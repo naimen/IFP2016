a2bf12d15247d27f55a96a0e27cabe565ef3e556
Shuo Hua <naimen12@gmail.com>
2016-10-26 12:09:14 +0200

    all folds

--- exam/RegexInterpreter.scm
+++ exam/RegexInterpreter.scm
@@ -415,20 +415,20 @@
   (lambda (reg vs)
     (letrec ([visit (lambda (r vs env k)
                       (cond
-                                        ;If r is empty, and vs is empty too, we should return an empty list
+;If r is empty, and vs is empty too, we should return an empty list
                         [(is-empty? r)
                          (k vs env 1)]
-                                        ;If r is atom, and the prefix of vs matches, we should return the rest of vs
+;If r is atom, and the prefix of vs matches, we should return the rest of vs
                         [(is-atom? r) 
                          (if (and (pair? vs)
                                   (= (car vs) (atom-1 r)))
                              (k (cdr vs) env 1)
                              0)]
-                                        ;If r is any, and the prefix of vs is a number, we should return the rest of vs
+;If r is any, and the prefix of vs is a number, we should return the rest of vs
                         [(is-any? r)
                          (if (pair? vs)
                              (k (cdr vs) env 1)
-                                        ;If r is seq, and vs is a pair, we should traverse the left side of vs, and the right side of vs. 
+;If r is seq, and vs is a pair, we should traverse the left side of vs, and the right side of vs. 
                              0)]
                         [(is-seq? r) 
                          (visit (seq-1 r) vs env
@@ -436,7 +436,7 @@
                                   (visit (seq-2 r) vs1 env1 
                                          (lambda (vs2 env2 c2)
                                            (k vs2 env2 (max c1 c2))))))]
-                                        ;If r is disj, in left most, we should first match on the right side of disj, if that fails, match on the left side of disj
+;If r is disj, in left most, we should first match on the right side of disj, if that fails, match on the left side of disj
                         [(is-disj? r)
                          (let* ([v1 (visit (disj-2 r) vs env k)]
                                 [v2 (visit (disj-1 r) vs env k)])
@@ -513,18 +513,18 @@
   (lambda (reg vs)
     (letrec ([visit (lambda (r vs env k)
                       (cond
-                                        ;If r is empty, and vs is empty too, we should return an empty list
+;If r is empty, and vs is empty too, we should return an empty list
                         [(is-empty? r)
                          (k vs env)]
-                                        ;If r is atom, and the prefix of vs matches, we should return the rest of vs
+;If r is atom, and the prefix of vs matches, we should return the rest of vs
                         [(is-atom? r) 
                          (if (and (pair? vs)
                                   (= (car vs) (atom-1 r)))
                              (k (cdr vs) env)
                              '())]
-                                        ;If r is any, and the prefix of vs is a number, we should return the rest of vs
+;If r is any, and the prefix of vs is a number, we should return the rest of vs
                         [(is-any? r)
-                                        ;If r is seq, and vs is a pair, we should traverse the left side of vs, and the right side of vs. 
+;If r is seq, and vs is a pair, we should traverse the left side of vs, and the right side of vs. 
                          (if (pair? vs)
                              (k (cdr vs) env)
                              '())]
@@ -532,7 +532,7 @@
                          (visit (seq-1 r) vs env
                                 (lambda (vs1 env1)
                                   (visit (seq-2 r) vs1 env1 k)))]
-                                        ;If r is disj, in left most, we should first match on the right side of disj, if that fails, match on the left side of disj
+;If r is disj, in left most, we should first match on the right side of disj, if that fails, match on the left side of disj
                         [(is-disj? r)
                          (let* ([v1 (visit (disj-2 r) vs env k)]
                                 [v2 (visit (disj-1 r) vs env k)])
@@ -998,6 +998,94 @@
 
 (unless (test-interpret-regular-expression-number interpret-regular-expression-numbers-result_fold-right)
   (printf "Result of numbers_foldright interpreter does not match the expected value"))
+
+
+;;;;;;;;;;;;;
+(define interpret-regular-expression-all-result_fold-right
+  (lambda (r vs)
+     ( ((fold-right-regular-expression
+        (lambda () ;empty
+          (lambda (vs env k)
+            (k vs env)))
+        (lambda (r) ;atom
+          (lambda (vs env k)
+            (if (and (pair? vs)
+                     (= (car vs) r))
+                (k (cdr vs) env)
+                '())))
+        (lambda () ;any
+          (lambda (vs env k)
+            (if (pair? vs)
+                (k (cdr vs) env)
+                '())))
+        (lambda (r1 r2) ;seq
+          (lambda (vs env k)
+            (r1 vs env
+                (lambda (vs1 env1)
+                  (r2 vs1 env1 k)))))
+        (lambda (r1 r2) ;disj
+          (lambda (vs env k)
+            (append (r2 vs env k)
+                    (r1 vs env k))))
+        (lambda (r) ;star
+          (lambda (vs env k)
+            (letrec ([loop (lambda (vs env)
+                             (append (r vs env (lambda (vs1 env1)
+                                                 (and (not (equal? vs vs1))
+                                                      (loop vs1 env1))))
+                                     (k vs env)
+                                     ))])
+              (loop vs env))))
+        (lambda (r) ;plus
+          (lambda (vs env k)
+            (letrec ([loop (lambda (vs env)
+                             (r vs env (lambda (vs1 env1)
+                                         (and (not (equal? vs vs1))
+                                              (append (loop vs1 env1)
+                                                      (k vs1 env1))))))])
+              (loop vs env))))
+        (lambda (r) ;var
+          (lambda (vs env k)
+            (letrec ([get-from-env
+                      (lambda (x env)
+                        (cond
+                          [(null? env)
+                           #f]
+                          [(and (pair? env)
+                                (pair? (car env)))
+                           (if (equal? (car (car env)) x)
+                               (cdr (car env))
+                               (get-from-env x (cdr env)))]
+                          [else
+                           #f]))])
+              (or (and (pair? vs)
+                       (or (and (not (get-from-env r (car env)))
+                                (k (cdr vs)
+                                   (cons (cons (cons r
+                                                     (car vs))
+                                               (car env))
+                                         (cdr env))))
+                           (and (equal? (get-from-env r (car env))
+                                        (car vs))
+                                (k (cdr vs) env))))
+                  '()))))
+        (lambda (r) ;else
+          (errorf
+           'interpret-regular-expression-right-most-result_fold-right
+           "Not a recognized expression, please consult BNF: ~s"
+           r)))
+        r)
+       vs '(()) (lambda (vs env)
+                (if (null? vs)
+                    env
+                    '()))
+       )))
+
+(unless (test-interpret-regular-expression-generic interpret-regular-expression-all-result_fold-right '())
+  (printf "Regex mismatch in all-results_foldright."))
+
+(unless (test-interpret-regular-expression-solutions interpret-regular-expression-all-result_fold-right)
+  (printf "Result of all-results_foldright interpreter does not match the expected value"))
 
 
 ;;;;;;;;;;;
